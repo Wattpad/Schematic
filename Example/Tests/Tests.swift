b81892408beca5d7871e05ea8fa85fa0795aa6b3
Elton Gao <gyfelton@gmail.com>
2019-12-06 11:31:31 -0500

    Added snapshot framworks for testing

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,28 +1,48 @@
-import XCTest
+import Quick
+import Nimble
 import Schematic
 
-class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+class TableOfContentsSpec: QuickSpec {
+    override func spec() {
+        describe("these will fail") {
+
+            it("can do maths") {
+                expect(1) == 1
+            }
+
+            it("can read") {
+                expect("number") == "number"
+            }
+
+            it("will eventually fail") {
+                expect("time").toEventually( equal("time") )
+            }
+
+            context("these will pass") {
+
+                it("can do maths") {
+                    expect(23) == 23
+                }
+
+                it("can read") {
+                    expect("🐮") == "🐮"
+                }
+
+                it("will eventually pass") {
+                    var time = "passing"
+
+                    DispatchQueue.main.async {
+                        time = "done"
+                    }
+
+                    waitUntil { done in
+                        Thread.sleep(forTimeInterval: 0.5)
+                        expect(time) == "done"
+
+                        done()
+                    }
+                }
+            }
         }
     }
-    
 }
